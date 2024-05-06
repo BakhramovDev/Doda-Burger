925214ee6b2ea42c1f99603fd68ed9ee252ba91a
BakhramovDev <143392013+BakhramovDev@users.noreply.github.com>
2024-05-06 16:07:38 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app_vue">
+    <headerNav />
+  </div>
+</template>
+
+<script> 
+import headerNav from "./components/Header.vue"
+export default {
+  components :{
+    headerNav
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- logo -->
+    <header>
+      <div class="header_wrapper">
+        <nav>
+          <!-- logo -->
+          <div class="logo">
+            <a href="../">
+              <b> Doda - Burger </b>
+            </a>
+          </div>
+          <!-- navlink -->
+
+          <ul class="nav_link">
+            <!--  -->
+            <li class="nav_item"><a href="#">Home</a></li>
+            <!--  -->
+            <li class="nav_item"><a href="#">Menu</a></li>
+            <!--  -->
+            <li class="nav_item"><a href="#">About</a></li>
+            <!--  -->
+            <li class="nav_item"><a href="#"></a></li>
+            <!--  -->
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </div>
+</template>
